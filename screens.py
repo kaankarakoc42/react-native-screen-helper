ea1576e16b98c0ccfb6a5c8280af2fb62ceea785
kaan karakoç <56826739+kaankarakoc42@users.noreply.github.com>
2021-01-29 21:24:35 +0300

    Update screens.py

--- screens.py
+++ screens.py
@@ -1,6 +1,42 @@
 import sys,os,shutil
 from glob import glob
+rcc="""
+import React, {{ Component }} from 'react';
+import {{ Text, View }} from 'react-native';
+import styles from './styles.js'
 
+class {0} extends Component {{
+  constructor(props) {{
+    super(props);
+    this.state = {{
+    }};
+  }}
+
+  render() {{
+    return (
+      <View style={{styles.container}}>
+        <Text> {0} </Text>
+      </View>
+    )
+  }}
+}}
+
+export default {0}
+
+                """
+styles="""import {StyleSheet,Dimensions} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+     flex:1,
+     justifyContent: "center",
+     alignItems: "center"
+  },
+});
+
+export default styles;                
+                
+                """
 class screens:
     def clearscreens(self):
         self.screens=[]
@@ -34,16 +70,17 @@ class screens:
 
             if self.args[i] == "del":
                 self.deleteScreen(self.setScreenName(self.args[i+1]))
-                
+
             if self.args[i] == "help" or len(self.args)==1:
                 print("add [screen name] adds new screen") 
                 print("fix [screen name] fix screens.js") 
-                print("del [screen name] delete screen")  
-            
+                print("del [screen name] delete screen")          
+                
     def deleteScreen(self,name):
          if os.path.exists(self.path+"/screens/"+name):
              shutil.rmtree(self.path+"/screens/"+name)
              self.fixScreens()
+             print(f"[+] {name} silindi.")
 
 
     def fixScreens(self):
@@ -54,56 +91,25 @@ class screens:
                 screens=imports+exports
                 f.write(screens)
 
-
-    def createScreen(self,name):
+    def control(self,name):
         for i in self.screens:
             if i == name:
                 print("[-] bu isim de bir ekran zaten oluşturulmuş.")
-                exit()
-        print(f"[+] {name} oluşturuluyor...")
+                return False
         if os.path.exists(self.path+"/screens"):
+            return True
+        else:
+            print(f"[-] screens dosyası bulunamadı.")
+            return False
+            
+    def createScreen(self,name):
+        if self.control(name):
             os.mkdir(self.path+"/screens/"+name)
             with open(self.path+"/screens/"+name+"/"+name+".js","w") as f:
-                f.write(f"""
-import React, {{ Component }} from 'react';
-import {{ Text, View }} from 'react-native';
-import styles from './styles.js'
-
-export class {name} extends Component {{
-  constructor(props) {{
-    super(props);
-    this.state = {{
-    }};
-  }}
-
-  render() {{
-    return (
-      <View>
-        <Text> {name} </Text>
-      </View>
-    )
-  }}
-}}
-
-export default {name}
-
-                """)
+                f.write(rcc.format(name))
             with open(self.path+"/screens/"+name+"/styles.js","w") as f:
-                f.write("""import {StyleSheet,Dimensions} from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-     flex:1,
-  },
-});
-
-export default styles;                
-                
-                """)
+                f.write(styles)
             self.fixScreens()
+            print(f"[+] {name} oluşturuldu.")
             
-            
-        
-
-
 app=screens()
